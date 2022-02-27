e5ee18af61592ad7e4e951322ab22f9a40e7f8db
khairul habibie S.Si <96696443+khairulhabibie@users.noreply.github.com>
2022-02-27 22:38:07 +0700

    Add files via upload

---
+++ 05_Data Query Language/tugas_5_Data Query Language.sql
@@ -0,0 +1,19 @@
+SELECT 
+	a.orderNumber,
+    a.orderDate,
+    b.customerName,
+    b.city,
+    b.country,
+    c.QuantityOrdered,
+    d.productName
+FROM 
+	orders a
+inner join customers b on a.customerNumber = b.customerNumber
+inner join orderdetails c on a.orderNumber = c.orderNumber
+inner join products d on c.productCode = d.productCode
+where  
+	a.orderDate >= '2004-08-01'
+	and a.orderDate <= '2004-12-01'
+    and d.productName = '1992 Ferrari 360 Spider red'
+order by b.country 
+;
